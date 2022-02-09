309ebf9d3afee797de391a720f64c7973793c71b
alequiro <escuartini@gmail.com>
2022-02-08 22:13:02 -0300

    barra de navegacio

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,4 +1,17 @@
 import React from 'react';
-import './navBar.css';
+import './NavBar.css';
 
-
+function Menu(){
+    return(
+        <nav class="navegador" >
+            <ul class= "lista">
+                <li><a href= "todo">TODO</a></li>
+                <li><a href= "ofertas">OFERTAS</a></li>
+                <li><a href= "lo mas vendido">LO MAS VENDIDO</a></li>
+                <li><a href= "ultimas novedades">ULTIMAS NOVEDADES</a></li>
+                <li><a href= "atencion al cliente">ATENCION AL CLIENTE</a></li>
+            </ul>
+        </nav>
+    )
+}
+export default Menu;
